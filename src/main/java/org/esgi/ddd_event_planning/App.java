5ebc13a9b59a2faa8d50f1ee559f08a8f4a93018
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-03-15 13:14:02 +0100

    init project

---
+++ src/main/java/org/esgi/ddd_event_planning/App.java
@@ -0,0 +1,10 @@
+package org.esgi.ddd_event_planning;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/org/esgi/ddd_event_planning/AppTest.java
@@ -0,0 +1,14 @@
+package org.esgi.ddd_event_planning;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AppTest {
+    @Test
+    void testApp() {
+        App.main(null);
+        assertTrue(true);
+    }
+}
