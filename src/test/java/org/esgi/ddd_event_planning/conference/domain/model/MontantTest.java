690f5b87d428df7859e382579e8aae931b7ae7bf
Swann HERRERA <swann@devloup.dev>
2023-03-17 11:13:13 +0100

    fix import

--- src/test/java/org/esgi/ddd_event_planning/conference/domain/model/MontantTest.java
+++ src/test/java/org/esgi/ddd_event_planning/conference/domain/model/MontantTest.java
@@ -1,6 +1,5 @@
 package org.esgi.ddd_event_planning.conference.domain.model;
 
-import org.esgi.ddd_event_planning.conference.domain.model.Montant;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
